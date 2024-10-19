0a232e12d38d3e3d9670835f75dd5a4fe2ba568c
Vitor Miconi <163423734+VitorMiconi@users.noreply.github.com>
2024-10-19 00:10:27 -0300

    gsap

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,25 @@
-gsap.to(".box", {
-    duration: 2,
-    x: 100,
-    yoyo: true,
-    repeat: -1,
-    ease: "power1.inOut",
-    backgroundColor: "#2ecc71",
-});
+const section = document.querySelector('.section'),
+    sectionContainer = section.querySelector('.section_container'),
+    sectionCol = section.querySelector('.section_col'),
+    sectionCaptions = section.querySelectorAll('.section_col_caption');
+
+const isDesktop = window.matchMedia('(min-width: 768px)');
+const init = ()=> {
+    if (isDesktop.matches) addEventListener()
+}
+
+const addEventListener = ()=> {
+    sectionCol[0].classList.add('active');
+
+    sectionCol.forEach((col, index)=> {
+        col.addEventListener('mouseenter', ()=>{
+            sectionCol.forEach((el) => el.classList.remove ('active'));
+
+            col.classList.add('active');
+
+            if (index === col.length - 1) col.classList.add ('active');
+        });
+    });
+};
+
+init();
